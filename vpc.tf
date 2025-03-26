0961b039593d23f670d12aec08ffe84ed2d4248f
Omkar Khatavkar <omkar.khatavkar@suse.com>
2025-03-26 17:18:08 +0530

    added the inbound rules for the instance and rancher manager

--- vpc.tf
+++ vpc.tf
@@ -55,10 +55,43 @@ resource "aws_security_group" "rancher_sg_allowall" {
   vpc_id      = aws_vpc.rancher_vpc.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP traffic"
+  }
+
+  ingress {
+    from_port   = 8088
+    to_port     = 8088
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow custom TCP traffic on port 8088"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH access"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTPS traffic"
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow custom TCP traffic on port 8000"
   }
 
   egress {
